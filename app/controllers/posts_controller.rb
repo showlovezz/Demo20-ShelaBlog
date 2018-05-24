aa6482a37cdf78a13e46de4c53e95f37db5fc901
vita <dragic.hsieh@gmail.com>
2018-05-24 20:32:55 +0800

    Debug controller and views page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	
 	def index
+		@posts = Post.all
 	end
 
 	def show
@@ -19,7 +20,7 @@ class PostsController < ApplicationController
 			redirect_to posts_path
 		else
 			flash[:alert] = "此文章建立失敗，請檢查"
-      render 'new'
+      		render 'new'
     end
 	end
 
