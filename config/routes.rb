db287d47c8c6dca3a9d994ced7ba62ae0daad530
vita <dragic.hsieh@gmail.com>
2018-05-24 19:32:42 +0800

    Done add posts controller and set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # 設定首頁
+  root "posts#index"
+  # 設定功能路由
+  resources :posts
+
 end
