ff38efef2215c1585fdc4252140baed0d6d3dd25
vita <dragic.hsieh@gmail.com>
2018-05-24 19:59:40 +0800

    Done add new、create action and page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,29 @@ class PostsController < ApplicationController
 	def show
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			flash[:notice] = "文章建立成功"
+			redirect_to posts_path
+		else
+			flash[:alert] = "此文章建立失敗，請檢查"
+      render 'new'
+    end
+	end
+
+
 	private
 
 	def set_post
 		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :body, :description)
 	end
 end
