65736ad9fe0626464aa4af6a90d32829ff70cf48
vita <dragic.hsieh@gmail.com>
2018-05-24 19:45:40 +0800

    Done add index、show action and page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+
+	before_action :set_post, only: [:show]
+	
+	def index
+	end
+
+	def show
+	end
+
+	private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
 end
