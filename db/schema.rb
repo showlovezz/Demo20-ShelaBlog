6c252f2a58c90397be7363c5a54bd4ad301124e2
vita <dragic.hsieh@gmail.com>
2018-05-24 23:52:18 +0800

    Debug underfined method slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524120958) do
+ActiveRecord::Schema.define(version: 20180524153429) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20180524120958) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
 end
