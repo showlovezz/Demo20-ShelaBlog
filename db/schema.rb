8fac15b295ebb29db5f053c539839d7f7157ef07
vita <dragic.hsieh@gmail.com>
2018-05-25 11:57:32 +0800

    Done add column image to posts table and add error messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524153429) do
+ActiveRecord::Schema.define(version: 20180525035344) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180524153429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "image"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
