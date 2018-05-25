8c5be3ea6338b9e160a3cec177a96929127c2c36
vita <dragic.hsieh@gmail.com>
2018-05-25 13:29:08 +0800

    Done add blog file and delete file...

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # 設定首頁
-  root "posts#index"
+  root "blog/posts#index"
   # 設定功能路由
-  resources :posts
+  scope module: "blog" do
+    resources :posts
+    get "about" => "pages#about", as: :about
+    get "contact" => "pages#contact", as: :contact
+  end
 
-  get "about" => "pages#about", as: :about
-  get "contact" => "pages#contact", as: :contact
+
 
 end
